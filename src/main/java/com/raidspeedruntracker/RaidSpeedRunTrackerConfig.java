dee4859903624af90e7aa6609fd0c3f5296965da
garryhuang1 <garryhuang1>
2021-09-05 22:45:25 -0400

    set up plugin

--- src/main/java/com/raidspeedruntracker/RaidSpeedRunTrackerConfig.java
+++ src/main/java/com/raidspeedruntracker/RaidSpeedRunTrackerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.raidspeedruntracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface RaidSpeedRunTrackerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
